e5ef35e77557268b24977c69093c1cf89bb757fa
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-03-19 14:20:50 +0100

    Update sprint16.js

--- sprint16.js
+++ sprint16.js
@@ -12,6 +12,7 @@ var fonctionnalite1 = creerBrancheDeFonctionnalite("f_C+Amelioration-Enrolement"
 var tache1 = creerBrancheDeTache(fonctionnalite1, "t_6917-certicode-plus-code-otp-sms");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_C+");
 var tache2 = creerBrancheDeTache(fonctionnalite2, "t_7814-tag-popin-selfcare");
+var fonctionnalite3 = creerBrancheDeFonctionnalite("f_gestion_cookies");
 
 
 // Branche SPRINT
