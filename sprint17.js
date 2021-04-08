976396a2891aa52475c3d3b3e5d79a44681fc1aa
MaximePic <maxime.pic@hotmail.fr>
2021-04-08 10:47:33 +0200

    Update sprint17.js

--- sprint17.js
+++ sprint17.js
@@ -11,7 +11,7 @@ integration.commit("start");
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_gestion_cookies");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "t-6396-gestion-cookies-mode-non-connecte");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_C+_twenty");
-var tache2 = creerBrancheDeTache(fonctionnalite1, "(t_8494-rationalisation-des-stripes");
+var tache2 = creerBrancheDeTache(fonctionnalite2, "(t_8494-rationalisation-des-stripes");
 
 // Branche SPRINT
 var brancheSprint = creerBrancheDeSprint("twenty_r01_s02 (21.2.0.215)");
