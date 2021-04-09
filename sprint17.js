38e7cd2d2e2ca31b4913f1553daa94f906ea06bb
jesterzon <julien.esterzon@gmail.com>
2021-04-09 12:32:53 +0200

    Update sprint17.js

--- sprint17.js
+++ sprint17.js
@@ -12,6 +12,7 @@ var fonctionnalite1 = creerBrancheDeFonctionnalite("f_gestion_cookies");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "t-6396-gestion-cookies-mode-non-connecte");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_C+_twenty");
 var tache2 = creerBrancheDeTache(fonctionnalite2, "(t_8494-rationalisation-des-stripes");
+var tache3 = creerBrancheDeTache(tache1, "t-7318-cookies-refonte-css-mode-connecte");
 
 // Branche SPRINT
 var brancheSprint = creerBrancheDeSprint("twenty_r01_s02 (21.2.0.215)");
