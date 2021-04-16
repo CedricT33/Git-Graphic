714c409c469ccdee30867aade3a96f9afd0eb00a
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-04-16 16:47:47 +0200

    Update sprint17.js

--- sprint17.js
+++ sprint17.js
@@ -29,7 +29,7 @@ var brancheSprint = creerBrancheDeSprint("twenty_r01_s02");
 // MERGES --------------------
 creerMerge(fonctionnalite1, tache1);
 creerMerge(fonctionnalite1, tache3);
-creerMerge(fonctionnalite5, integration);
+creerMerge(integration, fonctionnalite5);
 
 // FUSION APRES DEMO --------------------
 
