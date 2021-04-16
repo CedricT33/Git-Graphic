627a6e4e05a1e463d4c82018dcb5bf51479e6075
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-04-16 16:26:44 +0200

    Update sprint17.js

--- sprint17.js
+++ sprint17.js
@@ -18,6 +18,7 @@ var fonctionnalite3 = creerBrancheDeFonctionnalite("f_mandat_prelevement");
 var fonctionnalite4 = creerBrancheDeFonctionnalite("f_LBPF");
 var tache5 = creerBrancheDeTache(fonctionnalite4, "t_8408-lbpf-changement-urls");
 var tache6 = creerBrancheDeTache(fonctionnalite4, "t_7431-lbpf-evenement");
+var tache7 = creerBrancheDeTache(fonctionnalite4, "t_7656-retour-stripe-credit-renouvelable");
 var fonctionnalite5 = creerBrancheDeFonctionnalite("f_simplification_notifs_21_2");
 var anomalie1 = creerBrancheAnomalie(integration, "a_7325-smartblette-maj-app-profil");
 
