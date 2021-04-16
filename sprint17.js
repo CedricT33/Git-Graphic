5cc4161f6d685b74107956c3fdea70a7ee5dbcbe
FJarry <78537891+FJarry@users.noreply.github.com>
2021-04-16 16:05:58 +0200

    Update sprint17.js
    Report branche t_7431-lbpf-evenement du sprint16 (toujours en vie...)

--- sprint17.js
+++ sprint17.js
@@ -17,6 +17,7 @@ var tache4 = creerBrancheDeTache(fonctionnalite2, "t_8573-certicode-plus-renomma
 var fonctionnalite3 = creerBrancheDeFonctionnalite("f_mandat_prelevement");
 var fonctionnalite4 = creerBrancheDeFonctionnalite("f_LBPF");
 var tache5 = creerBrancheDeTache(fonctionnalite4, "t_8408-lbpf-changement-urls");
+var tache6 = creerBrancheDeTache(fonctionnalite4, "t_7431-lbpf-evenement");
 var fonctionnalite5 = creerBrancheDeFonctionnalite("f_simplification_notifs_21_2");
 var anomalie1 = creerBrancheAnomalie(integration, "a_7325-smartblette-maj-app-profil");
 
