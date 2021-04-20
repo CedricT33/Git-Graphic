f268ad3ff84092c6c2ef5dc014687423891b5a28
e-garnier <78742423+e-garnierSIO@users.noreply.github.com>
2021-04-20 14:22:41 +0200

    Update sprint17.js

--- sprint17.js
+++ sprint17.js
@@ -19,9 +19,10 @@ var fonctionnalite4 = creerBrancheDeFonctionnalite("f_LBPF");
 var tache5 = creerBrancheDeTache(fonctionnalite4, "t_8408-lbpf-changement-urls");
 var tache6 = creerBrancheDeTache(fonctionnalite4, "t_7431-lbpf-evenement");
 var tache7 = creerBrancheDeTache(fonctionnalite4, "t_7656-retour-stripe-credit-renouvelable");
+var anomalie2 = creerBrancheAnomalie(fonctionnalite4, "a_9143-LBPF-iframe-affichage");
 var fonctionnalite5 = creerBrancheDeFonctionnalite("f_simplification_notifs_21_2");
 var anomalie1 = creerBrancheAnomalie(integration, "a_7325-smartblette-maj-app-profil");
-var anomalie2 = creerBrancheAnomalie(integration, "a_9143-LBPF-iframe-affichage");
+
 
 // Branche SPRINT
 var brancheSprint = creerBrancheDeSprint("twenty_r01_s02");
