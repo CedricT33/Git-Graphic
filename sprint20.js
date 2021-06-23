9c8019b8bbe5bc6f87073aa9f30771c9ccebb117
Fillon Mathias <mathias.fillon@gmail.com>
2021-06-23 09:32:17 +0200

    ajout branche t_12259_mise_a_jour_stripes

--- sprint20.js
+++ sprint20.js
@@ -13,6 +13,8 @@ var fonctionnalite1 = creerBrancheDeFonctionnalite("f_migration_aem");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "t_7331-table-aem-hash");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_refonte_nav");
 var tache3 = creerBrancheDeTache(fonctionnalite2, "t_9366-accueil-vignette-comptes-agregateur");
+var tache4 = creerBrancheDeTache(fonctionnalite2, "t_12259_mise_a_jour_stripes");
+
 
 // MERGES --------------------
 //creerMerge(integration, fonctionnaliteXXX);
