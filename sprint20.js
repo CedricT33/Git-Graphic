29f13fea206bc4d90986045f3d790eac18c3d9e9
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-07-02 15:22:34 +0200

    Update sprint20.js

--- sprint20.js
+++ sprint20.js
@@ -26,5 +26,6 @@ var brancheSprint = creerBrancheDeSprint("twenty_r03_s01");
 creerMerge(fonctionnalite2, tache5);
 creerMerge(fonctionnalite2, tache7);
 creerMerge(fonctionnalite2, tache6);
+creerMerge(fonctionnalite2, tache3);
 //integration.commit("V21.03.00.XXX et XXX"); // tag
 
