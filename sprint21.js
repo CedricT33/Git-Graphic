450dba11afa5915faa3f528d408bdb0748add383
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-07-23 16:56:40 +0200

    Update sprint21.js

--- sprint21.js
+++ sprint21.js
@@ -13,15 +13,16 @@ var fonctionnalite1 = creerBrancheDeFonctionnalite("f_migration_aem");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "t_7331-table-aem-hash");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_refonte_nav");
 var tache3 = creerBrancheDeTache(fonctionnalite2, "t_13104_suppression_polling");
+var tache5 = creerBrancheDeTache(fonctionnalite2, "t_10000_integration_personnalisation");
 var fonctionnalite3 = creerBrancheDeFonctionnalite("f_certicode_plus");
 var tache2 = creerBrancheDeTache(fonctionnalite3, "t_11965-mise-en-avant-operations-certicode-plus");
 var tache4 = creerBrancheDeTache(integration, "t_7224-migration-aem");
-var tache5 = creerBrancheDeTache(fonctionnalite2, "t_10000_integration_personnalisation");
+
 
 // Branche SPRINT
 var brancheSprint = creerBrancheDeSprint("twenty_r03_s02");
 
 // MERGES --------------------
-//creerMerge(fonctionnaliteX, tacheX);
 creerMerge(fonctionnalite2, tache3);
+creerMerge(brancheSprint, fonctionnalite2);
 //integration.commit("V21.03.00.xxx et xxx"); // tag
