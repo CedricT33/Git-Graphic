14addf03773bbc053b7cfe5b20651935543b9b3c
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-08-23 15:42:22 +0200

    Update sprint23.js

--- sprint23.js
+++ sprint23.js
@@ -11,6 +11,7 @@ integration.commit("start");
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_refonte_nav");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "f_lwei_fusion_refonte_nav");
 var tache2 = creerBrancheDeTache(fonctionnalite1, "t_9910_adaptation_purge");
+var anomalie14429 = creerBrancheAnomalie(fonctionnalite1, "a_14429-tags-refonte-nav");
 var anomalie13679 = creerBrancheAnomalie(integration, "a_13679-loader-infini-op-vlider-touch-id");
 var tache3 = creerBrancheDeTache(integration, "t_14319_package_pour_IARD (ne pas fusionner)");
 var anomalie12604 = creerBrancheAnomalie(integration, "a_12604-tags-rationnalisation-stripes-certicode_plus");
