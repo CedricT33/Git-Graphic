d5654c95c3749a6e8d8054837d49697a13270af9
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-08-24 11:29:20 +0200

    Update sprint23.js

--- sprint23.js
+++ sprint23.js
@@ -9,7 +9,7 @@ integration.commit("start");
 
 // Branches FONCTIONNALITE, TACHES ET ANOS
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_refonte_nav");
-var tache1 = creerBrancheDeTache(fonctionnalite1, "f_lwei_fusion_refonte_nav");
+var tache1 = creerBrancheDeTache(fonctionnalite1, "f_lwei_fusion_refonte_nav (9869)");
 var tache2 = creerBrancheDeTache(fonctionnalite1, "t_9910_adaptation_purge");
 var anomalie14429 = creerBrancheAnomalie(fonctionnalite1, "a_14429-tags-refonte-nav");
 var anomalie13679 = creerBrancheAnomalie(integration, "a_13679-loader-infini-op-vlider-touch-id");
