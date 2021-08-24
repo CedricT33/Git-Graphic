7e2c301987fc5710413e7e4c9ccfc05024d65731
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-08-24 10:55:43 +0200

    Update sprint23.js

--- sprint23.js
+++ sprint23.js
@@ -19,6 +19,7 @@ var anomalie14072 = creerBrancheAnomalie(integration, "a_14072-apparition-furtiv
 
 
 // MERGES --------------------
+creerMerge(fonctionnalite1, tache1);
 creerMerge(fonctionnalite1, tache2);
 creerMerge(integration, anomalie12604);
 creerMerge(f_refonte_nav, tache2);
