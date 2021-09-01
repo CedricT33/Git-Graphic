be8ea2ef3bc214772cbca24d006cde7a626aad02
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-09-01 15:10:19 +0200

    Update sprint23.js

--- sprint23.js
+++ sprint23.js
@@ -39,7 +39,8 @@ creerMerge(integration, anomalie14040);
 creerMerge(integration, anomalie14419);
 integration.commit("V21.03.00.908 à 911"); // tag
 creerMerge(integration, anomalie14585);
-
+creerMerge(integration, anomalie14651);
+//integration.commit("V21.03.00.912 et 913"); // tag
 
 
 
