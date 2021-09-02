696a3ab380c928e7ebe535f832388211a46b963c
jesterzon <julien.esterzon@gmail.com>
2021-09-02 11:05:17 +0200

    Update sprint23.js

--- sprint23.js
+++ sprint23.js
@@ -42,7 +42,7 @@ creerMerge(integration, anomalie14585);
 creerMerge(integration, anomalie14651);
 creerMerge(integration, anomalie12604);
 integration.commit("V21.03.00.912 et 913"); // tag
-
+creerMerge(integration, anomalie13652);
 
 
 //-------------------------QH5---------------------------//
