09a6559674f38e3f40a18775f15ebc6d3bc717ca
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-09-16 12:14:36 +0200

    Update sprint24.js

--- sprint24.js
+++ sprint24.js
@@ -8,14 +8,15 @@ var integration = master.branch("integration_systemteam_r21_4");
 integration.commit("start");
 
 // Branches FONCTIONNALITE, TACHES ET ANOS
-//var anomalieXXX = creerBrancheAnomalie(fonctionnaliteXXX, "a_XXX");
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_formulaire_contestations_monetiques");
 var tache8999 = creerBrancheDeTache(integration, "t_8999_contestation_operation_carte");
+var anomalie5526 = creerBrancheAnomalie(integration, "a_5526_modification_tags");
 
 
 
 // MERGES --------------------
 creerMerge(fonctionnalite1, tache8999);
+creerMerge(integration, anomalie5526);
 
 
 
