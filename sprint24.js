32f3f72cd437a962bf1b539722df6e9a7c884052
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-09-28 10:55:49 +0200

    Update sprint24.js

--- sprint24.js
+++ sprint24.js
@@ -17,6 +17,9 @@ var tache1150 = creerBrancheDeTache(fonctionnalite2, "t_1150_spay_nouveaux_point
 var tache14533 = creerBrancheDeTache(integration, "t-14533-spay-debut-de-parcours-app");
 var tache12584 = creerBrancheDeTache(integration, "12584_cleanCodeCompte");
 var anomalie12604 = creerBrancheAnomalie(integration, "a_12604_Tag_CerticodePlus");
+var fonctionnalite3 = creerBrancheDeFonctionnalite("f_migration_aem");
+var tache7331 = creerBrancheDeTache(fonctionnalite3, "t_7331-table-aem-hash");
+var tache7224 = creerBrancheDeTache(fonctionnalite3, "t_7224-migration-aem");
 
 // Branche SPRINT
 var brancheSprint = creerBrancheDeSprint("twenty_r04_s01");
