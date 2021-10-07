d94ef6a9fa17881100929ac5012cb0f5069b4d14
jesterzon <julien.esterzon@gmail.com>
2021-10-07 15:48:09 +0200

    Update sprint25.js

--- sprint25.js
+++ sprint25.js
@@ -24,6 +24,7 @@ var brancheSprint = creerBrancheDeSprint("twenty_r04_s02");
 
 // MERGES --------------------
 //creerMerge(fonctionnaliteX, tacheX);
+creerMerge(fonctionnalite1, ano_15888);
 
 // FUSION --------------------
 
