2db3f4fc6d44d56b8168b0bda56b488b4d327756
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-10-19 08:09:53 +0200

    Update sprint25.js

--- sprint25.js
+++ sprint25.js
@@ -23,7 +23,7 @@ var tache7224 = creerBrancheDeTache(fonctionnalite2, "t_7224-migration-aem");
 var tache15785 = creerBrancheDeTache(fonctionnalite2, "t_15785-mentions-legales-via-aem");
 var tache15666 = creerBrancheDeTache(fonctionnalite2, "t_15666_AEM_contestation_monetique");
 var tache16228 = creerBrancheDeTache(fonctionnalite2, "t_16228_confirmation_enrolement");
-var tache15463 = creerBrancheDeTache(integration, "t_15463_notifs_connexion_invite");
+var tache15463 = creerBrancheAnomalie(integration, "t_15463_notifs_connexion_invite");
 
 // Branche SPRINT
 var brancheSprint = creerBrancheDeSprint("twenty_r04_s02");
