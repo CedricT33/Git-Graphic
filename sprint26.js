ff8d5861c42afd878a238355af2d32517597872d
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-10-28 15:15:00 +0200

    Update sprint26.js

--- sprint26.js
+++ sprint26.js
@@ -18,7 +18,7 @@ var tacheTestAem = creerBrancheDeMerge(integration, "ajout_page_test_aem");
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_ux_ui");
 var tache16160 = creerBrancheDeTache(fonctionnalite1, "t_16160_total_des_avoirs");
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_moteur_sensi_relance");
-var tache16349 = creerBrancheAnomalie(fonctionnalite2, "t_16349_ajout_popin_sensi_laf");
+var tache16349 = creerBrancheDeTache(fonctionnalite2, "t_16349_ajout_popin_sensi_laf");
 var tache15463 = creerBrancheAnomalie(integration, "t_16638");
 
 // Branche SPRINT
