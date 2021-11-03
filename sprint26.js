5230d0b4b8025ca8e3ef6ee93804380667a59335
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-11-03 10:37:43 +0100

    Update sprint26.js

--- sprint26.js
+++ sprint26.js
@@ -20,8 +20,8 @@ var tache16160 = creerBrancheDeTache(fonctionnalite1, "t_16160_total_des_avoirs"
 var fonctionnalite2 = creerBrancheDeFonctionnalite("f_moteur_sensi_relance");
 var tache16349 = creerBrancheDeTache(fonctionnalite2, "t_16349_ajout_popin_sensi_laf");
 var tache15463 = creerBrancheDeTache(integration, "t_16638");
-var fonctionnalite3 = creerBrancheDeFonctionnalite(integration, "f_13526_Apple_pay_uniformisation");
-var fonctionnalite4 = creerBrancheDeFonctionnalite(integration, "f_spay (16931)");
+var fonctionnalite3 = creerBrancheDeFonctionnalite("f_13526_Apple_pay_uniformisation");
+var fonctionnalite4 = creerBrancheDeFonctionnalite("f_spay (16931)");
 
 // Branche SPRINT
 var brancheSprint = creerBrancheDeSprint("twenty_r04_s03");
