6d3fe01b70d355fee1543cafcf58a0c489278c89
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-11-16 16:54:23 +0100

    Update sprint27.js

--- sprint27.js
+++ sprint27.js
@@ -29,16 +29,22 @@ var ano16829 = creerBrancheAnomalie(integration, "a_16829-spay-non-expiration-se
 var ano17332 = creerBrancheAnomalie(integration, "a_17332-parcours-spay-ccs");
 var ano17495 = creerBrancheAnomalie(integration, "a_17495");
 var tache16374 = creerBrancheDeTache(integration, "t_16374-spay-clean-code-poc");
+var ano17644 = creerBrancheAnomalie(integration, "a_17644-spay-time-out-5min");
+var ano17604 = creerBrancheAnomalie(integration, "a_17604-xpay-numeros-de-tel-ko");
+
 // Branche SPRINT
 
 
 // MERGES --------------------
+creerMerge(integration, ano17346);
 creerMerge(integration, ano16933);
 creerMerge(integration, ano16771);
 creerMerge(integration, ano16896);
 creerMerge(integration, ano16903);
 creerMerge(integration, ano16829);
 creerMerge(integration, ano17332);
-creerMerge(integration, ano17346);
+creerMerge(integration, tache16374);
+creerMerge(integration, ano17644);
+creerMerge(integration, ano17604);
 // FUSION --------------------
 
