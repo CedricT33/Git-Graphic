ae73da22d3cbf6259e90d3b866f13fba2334fda8
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2021-12-13 15:19:14 +0100

    Update sprint28.js

--- sprint28.js
+++ sprint28.js
@@ -22,6 +22,7 @@ integration.commit("start");
 // Branches FONCTIONNALITE, TACHES ET ANOS
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_rubrique_offre");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "t_19054-creation-de-la-rubrique-offres");
+var tache3 = creerBrancheDeTache(fonctionnalite1, "t_19054-rubrique-offres-menu-gerer");
 var tache2 = creerBrancheDeTache(integration, "t_17355-Mise_en_conformite_dsp2");
 var ano17495 = creerBrancheAnomalie(integration, "a_17495");
 var ano18811 = creerBrancheAnomalie(integration, "a_18811");
