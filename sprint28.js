06940050853aae5c78caf4cbd3eb74ace4309f4b
MaximePic <maxime.pic@hotmail.fr>
2021-12-16 10:55:41 +0100

    Update sprint28.js

--- sprint28.js
+++ sprint28.js
@@ -24,9 +24,12 @@ var fonctionnalite1 = creerBrancheDeFonctionnalite("f_rubrique_offre");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "t_19054-creation-de-la-rubrique-offres");
 var tache3 = creerBrancheDeTache(fonctionnalite1, "t_19054-rubrique-offres-menu-gerer");
 var tache2 = creerBrancheDeTache(integration, "t_17355-Mise_en_conformite_dsp2");
+var tache4 = creerBrancheDeTache(integration, "t_18468-rubrique-offres-niveau-1");
 var ano17495 = creerBrancheAnomalie(integration, "a_17495");
 var ano18811 = creerBrancheAnomalie(integration, "a_18811");
 var ano2792 = creerBrancheDeTache(integration, "t_2792-RefactoCarte");
 
 // MERGES --------------------
+creerMerge(tache4, tache1);
+creerMerge(tache4, tache3);
 creerMerge(integration, ano17495);
