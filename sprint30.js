348f6c7a7aa4d9167843285194b3a29146f43430
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2022-02-02 15:51:15 +0100

    Update sprint30.js

--- sprint30.js
+++ sprint30.js
@@ -20,14 +20,15 @@ integration.commit("start");
 
 // Branches FONCTIONNALITE, TACHES ET ANOS
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_rubrique_offre");
-var fonctionnalite2 = creerBrancheDeFonctionnalite("twenty_r01_s03");
 var tache1 = creerBrancheDeTache(fonctionnalite1, "t_18935-rubrique-assurances-prevoyance");
-creerMerge(fonctionnalite1, tache1);
-creerMerge(fonctionnalite1, fonctionnalite2);
 var tache2 = creerBrancheDeTache(fonctionnalite1, "t_21090-amelioration-sb-et-wording-niveau-1");
 var tache20333 = creerBrancheDeTache(fonctionnalite1, "t_20333-parcours-boquants-non-contact");
+var tache18913 = creerBrancheDeTache(fonctionnalite1, "t_18913-niveau3-pretimmo-creditconso");
 
 // MERGES --------------------
+var brancheSprint = creerBrancheDeSprint("twenty_r01_s03");
+creerMerge(fonctionnalite1, tache1);
+creerMerge(brancheSprint, fonctionnalite1);
 
 
 // FUSION --------------------
