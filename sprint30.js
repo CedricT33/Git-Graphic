9738b32e98e32dfbd7e0b7e208ba8658be909fff
jesterzon <julien.esterzon@gmail.com>
2022-02-03 13:50:30 +0100

    Update sprint30.js

--- sprint30.js
+++ sprint30.js
@@ -34,8 +34,8 @@ var brancheSprint = creerBrancheDeSprint("twenty_r01_s03");
 creerMerge(fonctionnalite1, tache1);
 var tache20733= creerBrancheDeTache(fonctionnalite1, "t_20733-bouchon-prod");
 var ano_21435 = creerBrancheAnomalie(fonctionnalite1, "a_21435-cta-decouvrir-iframe-lbpa");
-creerMerge(fonctionnalite1, ano_21435);
 creerMerge(brancheSprint, fonctionnalite1);
+creerMerge(fonctionnalite1, ano_21435);
 
 
 // FUSION --------------------
