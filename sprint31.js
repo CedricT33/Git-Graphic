404ad2375068a20ec61fe433604b4c3a9c2a0e3f
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2022-02-22 10:16:27 +0100

    Update sprint31.js

--- sprint31.js
+++ sprint31.js
@@ -20,6 +20,7 @@ integration.commit("start");
 
 // Branches FONCTIONNALITE, TACHES ET ANOS
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_rubrique_offre");
+var tache22276 = creerBrancheDeTache(fonctionnalite1, "t_22276-liens-externes-au-lieu-de-iframe");
 
 
 // MERGES --------------------
