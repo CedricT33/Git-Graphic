82f057e35244ad9adcf5e0c9d488010ad2c079cd
e-garnier <78742423+e-garnierSIO@users.noreply.github.com>
2022-02-24 09:25:08 +0100

    Update sprint31.js

--- sprint31.js
+++ sprint31.js
@@ -22,6 +22,7 @@ integration.commit("start");
 var fonctionnalite1 = creerBrancheDeFonctionnalite("f_rubrique_offre");
 var tache22276 = creerBrancheDeTache(fonctionnalite1, "t_22276-liens-externes-au-lieu-de-iframe");
 var tache21688 = creerBrancheDeTache(fonctionnalite1,"t_21688-images-dynamiques-niveau-1");
+var tache22508 = creerBrancheDeTache(fonctionnalite1,"tache/22508-liens-prod-recette");
 var ano_22164 = creerBrancheAnomalie(integration, "a_22164-accessibilite-offres-niveau-3-produit-intitule-des-boutons");
 var ano_22162 = creerBrancheAnomalie(integration, "fix/22162-piegeClavier");
 var ano_21169 = creerBrancheAnomalie(integration, "fix/21169-offres-accessibilite-gestion-du-focus");
