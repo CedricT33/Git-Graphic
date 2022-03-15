a29ad89a0e559bcddd5c8594bd8c40c46e914150
Fillon Mathias <mathias.fillon@gmail.com>
2022-03-15 10:09:54 +0100

    Update sprint31.js

--- sprint31.js
+++ sprint31.js
@@ -42,6 +42,7 @@ var ano_22276 = creerBrancheAnomalie(integration, "fix/22276-lien-credit-renouve
 var ano_23363 = creerBrancheAnomalie(integration, "fix/23363-top-pret-expresse-asynchrone");
 var tache_23197 = creerBrancheDeTache(integration, "feat/23197_Boite_outils_tests");
 var tache_21005 = creerBrancheDeTache(integration, "t_21005(poc sam)");
+var tache_23569 = creerBrancheDeTache(integration, "poc_23569_etude_browsertab");
 var fonctionnaliteTag = creerBrancheDeFonctionnalite("feat/tags");
 var tache22887 = creerBrancheDeTache(fonctionnaliteTag, "tache/22887-modification-js");
 var ano_23273 = creerBrancheAnomalie(integration, "fix/a_23273_annulation_virement");
