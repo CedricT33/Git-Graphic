8889f023b786b1ced4750973c87084e3ffe55c15
cibanez <clement.ibanez@free.fr>
2022-04-22 18:02:16 +0200

    Update sprint33.js

--- sprint33.js
+++ sprint33.js
@@ -31,6 +31,7 @@ var fonctionnalite5 = creerBrancheDeFonctionnalite("feat/rubrique_offre");
 var tache21499 = creerBrancheDeTache(fonctionnalite5, "tache/21499_rubrique_offres_debouchonnage");
 var tache23116 = creerBrancheDeTache(fonctionnalite5, "tache/23116_assurances_et_prevoyance_ameliorations");
 var fonctionnalite6 = creerBrancheDeFonctionnalite(, "feat/21666_offre_flux_v2");
+var fonctionnalite7 = creerBrancheDeFonctionnalite(, "poc_apple_pay (ne pas merger)");
 
 
 
