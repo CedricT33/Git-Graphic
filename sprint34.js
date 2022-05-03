a9cc2337d1ad6687ad6fe145e76116894f4ac7d1
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2022-05-03 07:28:51 +0200

    Create sprint34.js

---
+++ sprint34.js
@@ -0,0 +1,28 @@
+//----------DOCUMENTATION----------
+//var fonctionnaliteX = creerBrancheDeFonctionnalite("f_X");
+//var tacheX = creerBrancheDeTache(fonctionnaliteX, "t_X");
+//var ano_X = creerBrancheAnomalie(fonctionnaliteX, "a_X");
+//var brancheSprint = creerBrancheDeSprint("twenty_rXX_sXX");
+//var brancheMerge = creerBrancheDeMerge(integration, "merge_twenty_sX");
+//creerMerge(fonctionnaliteX, tacheX);
+//integration.commit("V22.01.00.9XX et 9XX"); // tag
+//---------------------------------
+
+// On instancie le graph dans le conteneur.
+var sprint34 = creerGitGraph("graph-container-34");
+
+// Branche MASTER & INTEGRATION
+var master = creerBrancheMaster(sprint34);
+
+var integration = master.branch("master (=22.2)");
+integration.commit("start");
+
+
+// Branches FONCTIONNALITE, TACHES ET ANOS
+
+
+
+// MERGES --------------------
+
+
+// FUSION --------------------
