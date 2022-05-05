b14e5e9a975a4a559aadec21a1e6db0fb706f0a6
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2022-05-05 13:41:31 +0200

    Update sprint34.js

--- sprint34.js
+++ sprint34.js
@@ -22,6 +22,8 @@ integration.commit("start");
 var fonctionnalite1 = creerBrancheDeFonctionnalite("feat/rubrique_offre")
 var tache22796 = creerBrancheDeTache(fonctionnalite1, "tache/22796-amelioration-visuels");
 var ano_24523  = creerBrancheAnomalie(integration, "fix/24523_erreur_500_faceid");
+var ano_24860  = creerBrancheAnomalie(integration, "fix/24860_loi_6902");
+
 
 // MERGES --------------------
 creerMerge(integration, ano_24523);
