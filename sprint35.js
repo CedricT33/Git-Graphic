f44982dbf8c3a9e5c77c1dc1782a599c94fc0b1c
CedricT33 <43745006+CedricT33@users.noreply.github.com>
2022-06-01 11:46:23 +0200

    Update sprint35.js

--- sprint35.js
+++ sprint35.js
@@ -44,6 +44,7 @@ creerMerge(integration, ano_25627);
 creerMerge(integration, ano_25907);
 creerMerge(integration, ano_25804);
 integration.commit("V22.02.00.908 et 909"); // tag
+creerMerge(integration, ano_25981);
 creerMerge(integration, ano_25914);
 
 
