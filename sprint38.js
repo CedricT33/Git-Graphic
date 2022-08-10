a8294dd7374609555aa9f7554b9b45fc54a56a7c
e-garnier <78742423+e-garnierSIO@users.noreply.github.com>
2022-08-10 16:43:59 +0200

    Update sprint38.js

--- sprint38.js
+++ sprint38.js
@@ -19,17 +19,18 @@ integration.commit("start");
 
 
 // Branches FONCTIONNALITE, TACHES ET ANOS
+var tacheCookies = creerBrancheDeTache(integration, "task/nouveau_fichier_js_cookies");
 var fonctionnaliteAPAY = creerBrancheDeFonctionnalite("feat/applepay");
 var tache25710 = creerBrancheDeTache(fonctionnaliteAPAY, "task/25710_choix_de_carte");
-var tacheCookies = creerBrancheDeTache(integration, "task/nouveau_fichier_js_cookies");
+var tache26287 = creerBrancheDeTache(fonctionnaliteAPAY, "task/26287_Nouveau_parcours_applePay");
+var ano27601 = creerBrancheDeTache(fonctionnaliteAPAY, "fix/27601_impossible_de_continuer_vers_le_parcours");
 var fonctionnaliteSAM2 = creerBrancheDeFonctionnalite("feat/SAM2");
 var fonctionnaliteSAM = creerBrancheDeFonctionnalite("feat/SAM");
 var tache27099 = creerBrancheDeTache(fonctionnaliteSAM, "task/integration-contralia-en-app (27099)");
 var tache27174 = creerBrancheDeTache(fonctionnaliteSAM, "task/27174_ecran_fin_parours (27174)");
 var tache27034 = creerBrancheDeTache(fonctionnaliteSAM, "task/27034_message_erreur_navigation");
 var fonctionnalite27571 = creerBrancheDeFonctionnalite("feat/27571_maj_version_automatique");
-var tache26287 = creerBrancheDeTache(fonctionnaliteAPAY, "task/26287_Nouveau_parcours_applePay");
-var ano27601 = creerBrancheDeTache(fonctionnaliteAPAY, "fix/27601_impossible_de_continuer_vers_le_parcours");
+
 
 
 // MERGES --------------------
