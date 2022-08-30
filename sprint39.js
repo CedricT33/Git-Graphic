158bd73129540544e611247d265d409b036320a6
cibanez <clement.ibanez@free.fr>
2022-08-30 11:24:33 +0200

    Update sprint39.js

--- sprint39.js
+++ sprint39.js
@@ -17,7 +17,6 @@ var master = creerBrancheMaster(sprint39);
 var integration = master.branch("master (=22.3)");
 integration.commit("start");
 
-
 // Branches FONCTIONNALITE, TACHES ET ANOS
 var ano_27330 = creerBrancheAnomalie(integration, "fix/27330_postmessages_manquants_iframe_lbpa");
 var ano_27994 = creerBrancheAnomalie(integration, "fix/27994_header_sam_contralia");
@@ -25,6 +24,9 @@ var ano_28142 = creerBrancheAnomalie(integration, "fix/28142_certicode_bouton_an
 var ano_28115 = creerBrancheAnomalie(integration, "fix/28115_apay_controle_sb_ko");
 var ano_27889 = creerBrancheAnomalie(integration, "fix/27889_accueil_non_connectee_accessibilite");
 var ano_28241 = creerBrancheAnomalie(integration, "fix/28241-apay-controle-ancien-parcours");
+var ano_28035 = creerBrancheAnomalie(integration, "fix/28035");
+var ano_25694 = creerBrancheAnomalie(integration, "fix/25694");
+var ano_27967 = creerBrancheAnomalie(integration, "fix/27967");
 
 // MERGES --------------------
 creerMerge(integration, ano_28142);
@@ -32,3 +34,20 @@ creerMerge(integration, ano_28115);
 creerMerge(integration, ano_27889);
 creerMerge(integration, ano_27994);
 creerMerge(integration, ano_28241);
+creerMerge(integration, ano_28035);
+creerMerge(integration, ano_25694);
+
+
+//-------------------------QH7---------------------------//
+
+// On instancie le graph dans le conteneur.
+var sprint39b = creerGitGraph("graph-container-39b");
+
+// Branche MASTER
+var masterQH7 = creerBrancheMasterQH7(sprint39b);
+masterQH7.commit("start");
+
+var tache28212 = creerBrancheDeMerge(masterQH7, "fix/28212");
+
+// MERGES --------------------
+creerMerge(masterQH7, tache24978);
