40e05a74400d5f1a01e4e05db9e58e62ac28fed0
Fillon Mathias <mathias.fillon@gmail.com>
2022-10-11 10:09:33 +0200

    Create sprin41.js

---
+++ sprin41.js
@@ -0,0 +1,39 @@
+//----------DOCUMENTATION----------
+//var fonctionnaliteX = creerBrancheDeFonctionnalite("f_X");
+//var tacheX = creerBrancheDeTache(fonctionnaliteX, "t_X");
+//var ano_X = creerBrancheAnomalie(fonctionnaliteX, "a_X");
+//var brancheSprint = creerBrancheDeSprint("twenty_rXX_sXX");
+//var brancheMerge = creerBrancheDeMerge(integration, "merge_twenty_sX");
+//creerMerge(fonctionnaliteX, tacheX);
+//integration.commit("V22.02.00.9XX et 9XX"); // tag
+//---------------------------------
+
+// On instancie le graph dans le conteneur.
+var sprint39 = creerGitGraph("graph-container-41");
+
+// Branche MASTER & INTEGRATION
+var master = creerBrancheMaster(sprint41);
+
+var integration = master.branch("master (=22.4)");
+integration.commit("start");
+
+var integration22_3 = master.branch("support/22.3");
+integration22_3.commit("start");
+
+// Branches FONCTIONNALITE, TACHES ET ANOS
+
+
+
+
+// MERGES --------------------
+
+
+
+//-------------------------QH7---------------------------//
+
+// On instancie le graph dans le conteneur.
+var sprint39b = creerGitGraph("graph-container-41b");
+
+// Branche MASTER
+var masterQH7 = creerBrancheMasterQH7(sprint41b);
+masterQH7.commit("start");
