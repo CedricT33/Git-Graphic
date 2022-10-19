c2ec1b48f9523b6872e97bc9067d0490db264154
jesterzon <julien.esterzon@gmail.com>
2022-10-19 17:14:58 +0200

    Update sprint41.js

--- sprint41.js
+++ sprint41.js
@@ -63,6 +63,11 @@ creerMerge(fonctionnaliteNouveauHeader, ano_29289);
 creerMerge(demo, fonctionnaliteNouveauHeader);
 creerMerge(demo, fonctionnaliteParapheur);
 
+creerMerge(fonctionnalitePARTENAIRES, fonctionnaliteSAMEPARGNE);
+creerMerge(fonctionnalitePARTENAIRES, fonctionnaliteParapheur);
+creerMerge(fonctionnalitePARTENAIRES, fonctionnaliteNouveauHeader);
+
+
 // fin 1er Merge --------------------
 
 var ano_29520 = creerBrancheAnomalie(fonctionnaliteNouveauHeader, "fix/29520_mauvais-routage-du-chevron-back");
